138efd511390b135a3abf5ff7d7625b6a2bf5df4
Vostryakov <Aleksandr.Vostriakov@leroymerlin.ru>
2020-05-26 11:05:27 +0300

    init commit

---
+++ src/main/java/com/femirion/rootthegame/RootTheGameApplication.java
@@ -0,0 +1,13 @@
+package com.femirion.rootthegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RootTheGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RootTheGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/femirion/rootthegame/RootTheGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.femirion.rootthegame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RootTheGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
